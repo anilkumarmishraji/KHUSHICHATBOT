3689e7f9c179728a31ca3aca5f1420c71432876d
anilkumarmishraji <anilkumarmishraji80@gmail.com>
2024-10-19 23:29:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "27345751"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "2f4074e9f5a56e99fd9ba562f2314523"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "1808943146"))
-SUPPORT_GRP = "TG_FRIENDSS"
-UPDATE_CHNL = "VIP_CREATORS"
-OWNER_USERNAME = "THE_VIP_BOY"
+OWNER_ID = int(getenv("OWNER_ID", "7812113897"))
+SUPPORT_GRP = "FRIEND_KI_MASTI"
+UPDATE_CHNL = "TG_style_name_bio"
+OWNER_USERNAME = "Vip_Raja_G"
